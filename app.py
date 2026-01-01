a0cee798007153d99de5b3b834ace8286b72ad7c
heisbeejay <banjidaniel143@gmail.com>
2026-01-02 00:11:30 +0100

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-
+import streamlit as st
+import pandas as pd
+st.set_page_config("page_title=Grid Failure Prediction", layout="wide")
+st.title("Grid Failure Prediction")
+st.write("Test to know next grid failure")
